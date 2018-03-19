d3c98c54910b0455b920408b5d1b1489f2455c31
N-T-SH <np2hf@virginia.edu>
2018-03-19 10:35:52 -0400

    Final commits.
    Removed KS and randomn selection button.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-Things not Working
-- Colors are still switching
-
-
 Things to be Added
 - Page Formatting
 - Threshold for triggering new distribution. Since we use the example of locating adversarial agents, we want to use a threshold at which the agent can react knowing that it will trigger a new distribution.
+- Statistical valididty of label switch solution.
---
+++ gmm_v3.py
@@ -0,0 +1,305 @@
+# Gaussian Mixture Models with a two centroid seeking agents
+# Kennan Grant, Nitesh Prakash
+# Machine Learning, Mid-Term Project
+# 3/18/2018
+
+import numpy as np
+import pandas as pd
+import random as rand
+import matplotlib.pyplot as plt
+from scipy.stats import stats
+from matplotlib.patches import Ellipse
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.mixture import GaussianMixture
+import random
+import math
+from scipy.stats import ks_2samp
+from scipy.spatial import distance
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+# Counter for no. of clicks on buttons
+noMoves = 0
+moves = 0
+resetCount = 0
+
+# Iteration Counter
+i = 0
+
+# Set initial values
+random.seed(1)
+max_iter = 100
+
+# Initial location of agents and distribution center
+
+agentR = (-25, -25) # initial location of agentR
+agentB = (25, -25) # initial location of agentB
+my_centers = ((-5, -5),(5, 5))
+
+# Global Variables to hold models and dataset
+gmm = None
+ks_df_OLD = None
+X_old = None
+Y_old = None
+B_mean_estimate = None
+R_mean_estimate = None
+
+# Function to calculate GMM and find the new location of the agents using a predetermined scaling factor 
+def estimateMeanConvergence(noMoveClick, moveClick, noOfResets, alpha, noOfSamples):
+    # declare the below variables as using global scope since they are reassigned in the function
+    global noMoves
+    global moves
+    global i
+    global resetCount
+    global my_centers
+    global agentR
+    global agentB
+    global max_iter
+    global gmm
+    global ks_df_OLD
+    global X_old
+    global Y_old
+    global B_mean_estimate
+    global R_mean_estimate
+
+    # indicator to check if KS statistic needs to be run
+    runKS = 0
+    # indicator to check if same distn. is selected
+    sameDist = 0
+    # Control indicator
+    control = 0
+    # initialize variables to initial values if reset button is pressed
+    if noOfResets>resetCount:
+        resetCount = noOfResets
+        
+        noMoves = 0
+        moves = 0
+        surprises = 0
+        i = 0
+        random.seed(1)
+        max_iter = 100
+        agentR = (-25, -25)
+        agentB = (25, -25)
+        my_centers = ((-5, -5),(5, 5))
+        gmm = None
+        ks_df_OLD = None
+        X_old = None
+        Y_old = None
+        B_mean_estimate = None
+        R_mean_estimate = None
+    
+    # Detect which button was pressed and generate data accordingly
+    if noMoveClick>noMoves:
+        move = 0
+        sameDist = 1
+        noMoves = noMoveClick
+    elif moveClick>moves:
+        move = 1
+        moves = moveClick
+    else:
+        print("Button press not detected. Assuming same distribution")
+        move = 0
+        sameDist = 1
+    print("iteration: %d"%i)
+    # assign center of distributions
+    my_centers = ((my_centers[0][0] + 1*move, my_centers[0][1] + 4*move),
+                  (my_centers[1][0] - 3*move, my_centers[1][1] - 1*move))
+    # draw samples
+    X, y_true = make_blobs(n_samples=int(noOfSamples), centers=my_centers,
+                       cluster_std=1.5, random_state=i)
+    
+    
+    # stack observations if new data from same distribution (in order to incorporate more data into estimate of mean).
+    # otherwise, only use new data.
+    #
+    # note: assumes independence of x and y
+    if i!= 0:
+        # fit GMM using previous means as initial means
+        gmm = GaussianMixture(n_components=2, means_init = gmm.means_,
+                          max_iter=max_iter).fit(X)
+        # extract predicted labels
+        labels = gmm.predict(X)
+        ks_df = pd.DataFrame(np.column_stack((X, labels, y_true)))
+        ks_df.columns=['x1', 'x2', 'labels','true_labels']
+        
+        # if same distribution is selected stack data
+        if sameDist == 1:
+            # stack observations
+            print("Same Distribution: Button")
+            X = np.vstack((X_old, X))
+            y_true = np.concatenate((Y_old, y_true), axis = 0)
+    # fit GMM
+    gmm = GaussianMixture(n_components=2, init_params='kmeans',
+                          max_iter=max_iter).fit(X)
+    
+    # extract predicted labels
+    labels = gmm.predict(X)
+    
+    # Calculate distance between old and new means 
+    if i!=0:
+        R_new_mean_estimate, B_new_mean_estimate = tuple(gmm.means_[0]), tuple(gmm.means_[1])
+        dst_B = distance.euclidean(B_new_mean_estimate,B_mean_estimate)
+        dst_R = distance.euclidean(R_new_mean_estimate,R_mean_estimate)
+        print("Distance between means: ",dst_B,dst_R)
+        # B_mean_estimate, R_mean_estimate = B_new_mean_estimate, R_new_mean_estimate
+        # If distance is high switch labels
+                
+        if (dst_B > 5) or (dst_R > 5):
+            print("switch")
+            labels = abs(labels-1)
+            control = 1
+            
+    # create ks df for next iteration
+    ks_df_OLD = pd.DataFrame(np.column_stack((X, labels, y_true)))
+    ks_df_OLD.columns=['x1', 'x2', 'labels','true_labels']
+    
+    # update target means for agents to seek
+    if control == 1:
+        B_mean_estimate, R_mean_estimate = tuple(gmm.means_[0]), tuple(gmm.means_[1])
+    else:
+        R_mean_estimate, B_mean_estimate = tuple(gmm.means_[0]), tuple(gmm.means_[1])
+    
+    # calc distances from agents to respective means
+    distanceR = (sum((np.array(R_mean_estimate) - np.array(agentR))**2))**(1/2) # unweighted....no log prob...
+    distanceB = (sum((np.array(B_mean_estimate) - np.array(agentB))**2))**(1/2) # unweighted....no log prob...
+    
+    # calculate angles
+    angle_degreeR = math.degrees(math.atan2(R_mean_estimate[1] - agentR[1],
+                                            R_mean_estimate[0] - agentR[0]))
+    angle_degreeB = math.degrees(math.atan2(B_mean_estimate[1] - agentB[1],
+                                            B_mean_estimate[0] - agentB[0]))
+    
+    # scale (if set to 1, agent will move all the way to mean of respective distribution) 
+    # replaced .5 with alpha as the learning rate
+    scaleR = alpha
+    scaleB = alpha
+    
+    # set new agent location (red)
+    agentR = agentR[0] + scaleR * distanceR * math.cos(angle_degreeR * math.pi / 180),\
+             agentR[1] + scaleR * distanceR * math.sin(angle_degreeR * math.pi / 180)
+    # set new agent location (blue)
+    agentB = agentB[0] + scaleB * distanceB * math.cos(angle_degreeB * math.pi / 180),\
+             agentB[1] + scaleB * distanceB * math.sin(angle_degreeB * math.pi / 180)
+    
+    # make data copy for next iteration
+    X_old = np.copy(X)
+    Y_old = np.copy(y_true)
+    
+    #Increment Counter
+    i+=1
+
+def plotData(ks_df_OLD, agentR, agentB):
+    # Function that, given the distribution dataframe, and the agents
+    # location returns plot information.
+    
+    dataRPlot = go.Scatter(
+        x=ks_df_OLD[ks_df_OLD['labels'] == 0]['x1'],
+        y=ks_df_OLD[ks_df_OLD['labels'] == 0]['x2'],
+        mode='markers',
+        opacity=0.7,
+        marker={
+            'color': 'pink',
+            'size': 15,
+            'line': {'width': 0.5, 'color': 'white'}
+        },
+        name='Red'
+    )
+    dataBPlot = go.Scatter(
+        x=ks_df_OLD[ks_df_OLD['labels'] == 1]['x1'],
+        y=ks_df_OLD[ks_df_OLD['labels'] == 1]['x2'],
+        mode='markers',
+        opacity=0.7,
+        marker={
+            'color': 'turquoise',
+            'size': 15,
+            'line': {'width': 0.5, 'color': 'white'}
+        },
+        name='Blue'
+    )
+    agentRPlot = go.Scatter(
+        x=[agentR[0]],
+        y=[agentR[1]],
+        mode='markers',
+        marker={
+            'color': 'red',
+            'size': 15,
+            'line': {'width': 0.5, 'color': 'white'}
+        },
+        name='Red Agent'
+    )
+    agentBPlot = go.Scatter(
+        x=[agentB[0]],
+        y=[agentB[1]],
+        mode='markers',
+        marker={
+            'color': 'blue',
+            'size': 15,
+            'line': {'width': 0.5, 'color': 'white'}
+        },
+        name='Blue Agent'
+    )
+    
+    data = [dataRPlot, dataBPlot, agentRPlot, agentBPlot]
+    
+    layout =  go.Layout(
+        xaxis={'title': 'X1'},
+        yaxis={'title': 'X2'},
+        height = 500, width =500,
+        margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+        legend={'x': 0, 'y': 1},
+        hovermode='closest'
+    )
+    
+    fig = dict(data=data, layout=layout)
+    return fig
+
+
+# Invoking Dash app
+app = dash.Dash()
+
+# Dash app components
+app.layout = html.Div([
+    html.H3(id='title', children = 'Tracking Dynamic Distributions: Label Switching in Gaussian Mixture Modeling'),
+    dcc.Graph(
+        id='GMM with Agent Model'
+    ),
+    html.Button(id='no-move', n_clicks=0, children='Same Distribution'),
+    html.Button(id='move', n_clicks=0, children='Different Distribution'),
+    html.Button(id='reset', n_clicks=0, children='Reset'),
+    dcc.Input(id='sample-size', type='number', value='15'),
+    html.Button(id='submit-button', n_clicks=0, children='Submit - Sample Size'),
+    html.Div(id='slider-descr', children = 'Select Learning Rate'),
+    dcc.Slider(
+        id='alpha-slider',
+        min=0,
+        max=1,
+        value=0.5,
+        step=0.1,
+        marks = {0:'0', 0.5:'0.5', 1:'1'}
+    )
+])
+# Invoke the callbacks from user input  
+@app.callback(
+    dash.dependencies.Output('GMM with Agent Model', 'figure'),
+    [dash.dependencies.Input('no-move', 'n_clicks'),
+     dash.dependencies.Input('move', 'n_clicks'),
+     dash.dependencies.Input('reset', 'n_clicks'),
+     dash.dependencies.Input('alpha-slider', 'value'),
+     dash.dependencies.Input('submit-button', 'n_clicks')],
+     [dash.dependencies.State('sample-size', 'value')]
+)
+# All inputs from the callback are passed into this function
+def generateData(noMoveClick, moveClick, noOfResets, alpha, submitClicks, noOfSamples):
+    print("App Inputs: noMoveClick, moveClick, noOfResets, alpha, submitClicks, noOfSamples")
+    print(noMoveClick, moveClick, noOfResets, alpha, submitClicks, noOfSamples)
+    estimateMeanConvergence(noMoveClick, moveClick, noOfResets, alpha, noOfSamples)
+    # Rename dataframe columns
+    return plotData(ks_df_OLD, agentR, agentB)
+
+# Run dash app
+if __name__ == '__main__':
+    app.run_server(debug = False)
